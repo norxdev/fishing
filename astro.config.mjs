5b911ecd8acabebd441c0e1c2d0df66f354eef18
norxdev <norxonics@gmail.com>
2026-01-10 18:17:39 -0500

    Fix base path format in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://norxdev.github.io",
-  base: "/fishing",
+  base: "/fishing/",
   markdown: {
     drafts: false
   }
