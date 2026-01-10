fa3954c58dc7555bf2912cd438ee6a82b4682e6b
norxdev <norxonics@gmail.com>
2026-01-10 18:06:46 -0500

    Update site URL and set base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://norxdev.github.io/fishing",
+  site: "https://norxdev.github.io",
+  base: "/fishing",
   markdown: {
     drafts: false
   }
