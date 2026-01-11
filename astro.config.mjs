50e3806f9e32eeb4055a3792db8079613e13fb03
norxdev <norxonics@gmail.com>
2026-01-11 16:46:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://norxdev.github.io', // GitHub Pages domain
-  base: '/fishing/',                 // Include trailing slash for GitHub Pages
-  output: 'static',                  // Static site output
+  site: "https://norxdev.github.io",
+  base: "/fishing/",
+  output: "static",
 });
